1b090be643f8b4f07695b6d7cd73ca1bcbb301b4
Nikolay Kalashnikov <kalash-job@yandex.ru>
2022-04-24 23:37:35 +0300

    fix tests (power_assert did not work)

--- test/test_ipgeobase.rb
+++ test/test_ipgeobase.rb
@@ -4,34 +4,37 @@ require_relative "test_helper"
 require_relative "../lib/ipgeobase"
 
 class TestIpgeobase < Minitest::Test
-  PREPARED_JSON_RESPONSE = '{"status":"success","country":"United States","countryCode":"US",
-    "region":"VA","regionName":"Virginia","city":"Ashburn","zip":"20149","lat":39.03,"lon":-77.5,
-    "timezone":"America/New_York","isp":"Google LLC","org":"Google Public DNS","as":"AS15169 Google LLC",
-    "query":"8.8.8.8"}'
-
   def setup
     stub_request(:get, "http://ip-api.com/json/8.8.8.8")
-      .to_return PREPARED_JSON_RESPONSE
+      .to_return(body: File.new("example_curl_response.json").read, status: 200)
     @meta_data = Ipgeobase.lookup("8.8.8.8")
   end
 
   def test_city
-    assert { @meta_data.city == "Ashburn" }
+    puts @meta_data.city
+    puts @meta_data.country
+    puts @meta_data.country_code
+    assert_equal("Ashburn", @meta_data.city)
+    # assert { @meta_data.city == "Ashburn" }
   end
 
   def test_country
-    assert { @meta_data.country == "United States" }
+    assert_equal("United States", @meta_data.country)
+    # assert { @meta_data.country == "United States" }
   end
 
   def test_country_code
-    assert { @meta_data.country_code == "US" }
+    assert_equal("US", @meta_data.country_code)
+    # assert { @meta_data.country_code == "US" }
   end
 
   def test_lon
-    assert { @meta_data.lon == "-77.5" }
+    assert_equal("-77.5", @meta_data.lon)
+    # assert { @meta_data.lon == "-77.5" }
   end
 
   def test_lat
-    assert { @meta_data.lat == "39.03" }
+    assert_equal("39.03", @meta_data.lat)
+    # assert { @meta_data.lat == "39.03" }
   end
 end
